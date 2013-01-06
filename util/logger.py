fb8d873a90be7d2443f0c5e27f7949e2fe49fc3e
AntiTree <pi@raspberrypi.(none)>
2013-01-06 18:14:40 +0000

    revamped for simple logging

--- util/logger.py
+++ util/logger.py
@@ -4,26 +4,26 @@ import multiprocessing
 import time, datetime
 import sqlite3
 
-DATABASE = 'doorberrypi.db'
 
 class Logging(object):
 
-   def __init__(self ):
-      print("Some shit")
+   def __init__(self, database ):
+      conn = sqlite3.connect(database)
+      conn.execute('CREATE TABLE IF NOT EXISTS log(date text, level text, action text, uid text)')
+      conn.commit()
+      self.conn = conn
+      
 
    def log(self, action, uid=None, level="INFO" ):
       """Function that listens for the type of event (ie Door open) and optionally, 
       the user associated to the activity (ie ibutton ID). The timestamp is automatically generated"""
-      proc_name = multiprocessing.current_process().name
+
 
       #connect to sqlite database
-      conn = sqlite3.connect(DATABASE)
-      cur = conn.cursor()
       timestamp = str(datetime.datetime.now())
       sql = 'INSERT INTO log VALUES(?, ?, ?, ?)'
-      cur.execute(sql, level, action, uid, timestamp)
-      cur.commit()
-      conn.close()
+      self.conn.execute(sql, (timestamp, level, action, uid))
+      self.conn.commit()
 
    def queue(self):
       self.queue = multiprocessing.Queue()
@@ -35,30 +35,12 @@ def worker(q):
    obj.log()
 
 if __name__ == '__main__':
-   queue = multiprocessing.Queue()
+   #queue = multiprocessing.Queue()
 
-   p = multiprocessing.Process(target=worker, args=(queue,))
-   p.start()
+   #p = multiprocessing.Process(target=worker, args=(queue,))
+   #p.start()
 
    #queue.put(Logging('Door Open', "Mark"))
-   log = Logging()
-   log.log("Door open", "Mark")
+   logs = Logging(DATABASE)
+   logs.log("Door open", "Mark")
 
-
-   queue.close()
-   queue.join_thread()
-   p.join()
-#sqlite connection
-
-def sqliteconnect():
-  conn = sqlite3.connect(DATABASE)
-  cur = conn.cursor()
-  return conn,cur
-
-#sqlite table setup
-
-
-#sqlite adding content
-
-
-#maintaining state
